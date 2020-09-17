4934d6b42bc34f9dc018c2e6d147e226f889f347
Hanyu Chen <chenhy19980116@gmail.com>
2020-09-17 21:04:14 +0800

    exercise2 pass

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,28 @@
+# import pandas as pd
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    stopWords = ["\n", "\""]
+    f = open(csv_file_path, "r")
+    res = [line for line in f.readlines()]
+
+    # Delete stopWords (like punctuations except ",")
+    for sw in stopWords:
+        res = [line.replace(sw, "") for line in res]
+
+    # Split each element
+    # If there is a integer, convert it from str to int
+    for i in range(len(res)):
+        tmp = res[i].split(",")
+        for j in range(len(tmp)):
+            try:
+                tmp[j] = int(tmp[j])
+            except:
+                pass
+        res[i] = tmp
+    return res
+
+# read_csv("../tests/test_files/dataset_2.csv")
+
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,40 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += (x[i]-y[i]) ** 2
+    return res ** 0.5
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += x[i]<y[i] and y[i]-x[i] or x[i]-y[i]
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    # Check if the lengths of input are the same or equal 0
+    if len(x) != len(y) or len(x) == 0:
+        # -1 means the input is not right
+        return -1;
+    disjunction = 0;
+    conjunction = 0;
+    for i in range(len(x)):
+        if x[i] == y[i]:
+            conjunction += 1;
+        disjunction += 1;
+    return 1 - conjunction/disjunction;
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    # Check if the lengths of input are the same or equal 0
+    if len(x) != len(y) or len(x) == 0:
+        # -1 means the input is not right
+        return -1;
+    divisor = 0;
+    dividend1 = 0;
+    dividend2 = 0;
+    for i in range(len(x)):
+        divisor += x[i] * y[i];
+        dividend1 += x[i] ** 2;
+        dividend2 += y[i] ** 2;
+    return divisor/(dividend1 ** 0.5 * dividend2 ** 0.5)
 
 # Feel free to add more
